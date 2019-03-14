61ac5c96ab50e2eb86657db63715ab1b2bd658e5
M.T <bmthang94@gmail.com>
2019-03-14 17:16:57 +0700

    Update new object structure

--- js/textfield.js
+++ js/textfield.js
@@ -1,20 +1,38 @@
 var tweetInput = document.getElementById('tweet-input');
 var charRemaining = document.getElementById('char-remaining');
 var tweetBtn = document.getElementById('tweet-btn');
-var tweetList = document.getElementById('tweet-list'); 
+var tweetList = document.getElementById('tweet-list');
 
-var max_value; 
-let tweetListArr =[{
-	id: '',
-	content:'',
-	publishAt: '',
-	retweetId: '',
-	like:'',
-	retweetTimes: ''
+var max_value;
+let tweetListArr = [{
+    id: 1,
+    user: {
+        name: 'Tim Cook',
+        avartar: 'https://pbs.twimg.com/profile_images/1035649273721847809/B0f8n_oe_bigger.jpg'
+    },
+    content: 'It’s not too late to get moving for #heartmonth! Thanks to everyone who joined our #TodayatApple Health & Fitness Walk in Brooklyn yesterday. #closeyourrings',
+    publishAt: '11:49 AM - 22 Feb 2019',
+    retweetId: '',
+    like: '2900',
+    retweetTimes: '302',
+    imageUrl: 'https://pbs.twimg.com/media/D1KPPOqU0AAEoVX.jpg'
+},
+{
+    id: 2,
+    user: {
+        name: 'Sundar Pichai',
+        avartar: 'https://pbs.twimg.com/profile_images/864282616597405701/M-FEJMZ0_bigger.jpg'
+    },
+    content: 'I love Pi-loi team',
+    publishAt: '11:49 AM - 22 Feb 2019',
+    retweetId: '',
+    like: '5000',
+    retweetTimes: '582',
+    imageUrl: 'https://pbs.twimg.com/media/D1H_sjeUYAAvi7-.jpg'
 }];
 
-tweetBtn.addEventListener('click', function() {
-	tweetList.innerHTML +=  `<div class="panel-body" id="tweets-list">
+tweetBtn.addEventListener('click', function () {
+    tweetList.innerHTML += `<div class="panel-body" id="tweets-list">
 					<div class="media">
 						<a class="media-left" href="#fake">
 							<img alt="" class="media-object-img-rounded" height = 30px width = 30px src="images/human.png">
@@ -35,22 +53,22 @@ tweetBtn.addEventListener('click', function() {
 });
 
 function resetField() {
-	tweetInput.value = '';
-	
+    tweetInput.value = '';
+
 }
 
 
-tweetInput.addEventListener('input', function() {
-	max_value = 140 - tweetInput.value.length;
-	charRemaining.innerHTML = max_value + " Characters Remaining";
-	
-	if (max_value < 0) {
-		charRemaining.style.color = 'red';
-		tweetBtn.disabled = true;
-	} else {
-		
-		charRemaining.style.color = 'black';
-		tweetBtn.disabled = false;
-	}
+tweetInput.addEventListener('input', function () {
+    max_value = 140 - tweetInput.value.length;
+    charRemaining.innerHTML = max_value + " Characters Remaining";
+
+    if (max_value < 0) {
+        charRemaining.style.color = 'red';
+        tweetBtn.disabled = true;
+    } else {
+
+        charRemaining.style.color = 'black';
+        tweetBtn.disabled = false;
+    }
 
 });// Code here
