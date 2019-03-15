1b926c02422ca557d9573d1514d7e8a78cbf6d6c
M.T <bmthang94@gmail.com>
2019-03-15 12:00:28 +0700

    udpate, push new tweet to current tweetListArr

--- js/textfield.js
+++ js/textfield.js
@@ -32,24 +32,38 @@ let tweetListArr = [{
 }];
 
 tweetBtn.addEventListener('click', function () {
-    tweetList.innerHTML += `<div class="panel-body" id="tweets-list">
-					<div class="media">
-						<a class="media-left" href="#fake">
-							<img alt="" class="media-object-img-rounded" height = 30px width = 30px src="images/human.png">
-						</a>
-						<div class="media-body">
-							<p id='body'>${tweetInput.value}</p>
+    let newTwsid = parseInt(getLastedId()) + 1;
+    tweetList.innerHTML += `
+    <div class="panel-body" id="tweets-list-${newTwsid}">
+        <div class="media">
+            <a class="media-left" href="#fake">
+                <img alt="" class="media-object-img-rounded" height = 30px width = 30px src="images/human.png">
+            </a>
+            <div class="media-body">
+                 <p id='body'>${tweetInput.value}</p>
 
-							<ul class="nav nav-pills nav-pills-custom">
-								<li> <a id="retweet-btn" href="#"><span class="glyphicon glyphicon-retweet"> Retweet </span></a> </li>
-								<li> <a id="like-btn" href="#"><span class="glyphicon glyphicon-star"> Like </span></a> </li>
-								<li> <a id="delete-btn" href="#" style="color:red"><span class="glyphicon glyphicon-delete"> Delete </span></a> </li>
-							</ul>
-						</div>
+                <ul class="nav nav-pills nav-pills-custom">
+                    <li> <a id="retweet-btn" href="#"><span class="glyphicon glyphicon-retweet"> Retweet </span></a> </li>
+                    <li> <a id="like-btn-${newTwsid}" onclick="likeTws(${newTwsid})" href="#"><span class="glyphicon glyphicon-star"> 0 Like</span></a> </li>
+                    <li> <a id="delete-btn-${newTwsid}" onclick="deleteTws(${newTwsid})" href="#" style="color:red"><span class="glyphicon glyphicon-delete"> Delete </span></a> </li>
+                </ul>
+            </div>
+        </div>
+    </div>`
 
-					</div>
-				</div>
-			</div>`
+    tweetListArr.push({
+        id: newTwsid,
+        user: {
+            name: 'Me',
+            avartar: 'https://pbs.twimg.com/profile_images/1035649273721847809/B0f8n_oe_bigger.jpg'
+        },
+        content: tweetInput.value,
+        publishAt: new Date(),
+        retweetId: null,
+        like: 0,
+        retweetTimes: 0,
+        imageUrl: ''
+    })
 });
 
 function resetField() {
@@ -71,4 +85,8 @@ tweetInput.addEventListener('input', function () {
         tweetBtn.disabled = false;
     }
 
-});// Code here
+});
+
+let getLastedId = () => {
+    return tweetListArr[tweetListArr.length - 1].id;
+}
